53951a5ac215870a665b7a1229dd023341ba8995
k-javaman <kjavman12@gmail.com>
2023-05-28 01:25:43 +0900

    add all modals on roadmap

--- src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/CsController.java
+++ src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/CsController.java
@@ -6,9 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CsController {
-    @GetMapping("/CS/")
-    public String htmlPage(Model model) {
+    @GetMapping("/CS/crash")
+    public String crashPage(Model model) {
         // Pass any specific data to HTML page
-        return "/CS/Crash";
+        return "CS/crash";
+    }
+
+    @GetMapping("/CS/cs50")
+    public String cs50Page(Model model) {
+        // Pass any specific data to HTML page
+        return "CS/cs50";
+    }
+
+    @GetMapping("/CS/nand-dsa")
+    public String nandDsaPage(Model model) {
+        // Pass any specific data to HTML page
+        return "CS/nand-dsa";
+    }
+
+    @GetMapping("/CS/cs-os-db-net")
+    public String CaOsDbNetPage(Model model) {
+        // Pass any specific data to HTML page
+        return "CS/ca-os-db-net";
+    }
+
+    @GetMapping("/CS/interview")
+    public String interview(Model model) {
+        // Pass any specific data to HTML page
+        return "CS/interview";
     }
 }
--- src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/LanguageController.java
+++ src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/LanguageController.java
@@ -3,13 +3,54 @@ package com.frontbackend.thymeleaf.bootstrap.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LanguageController {
-    @GetMapping("/Language/")
+    @GetMapping("/Language/HTML")
     public String htmlPage(Model model) {
         // Pass any specific data to HTML page
         return "/Language/HTML";
+    }
+
+    @GetMapping("/Language/java")
+    public String javaPage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/java";
+    }
+
+    @GetMapping("/Language/mysql")
+    public String mysqlPage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/mysql";
+    }
+
+    @GetMapping("/Language/spring")
+    public String springPage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/spring";
+    }
+
+    @GetMapping("/Language/jpa")
+    public String jpaPage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/jpa";
+    }
+
+    @GetMapping("/Language/spring-security")
+    public String securityPage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/spring-security";
+    }
+
+    @GetMapping("/Language/spring-unit-testing")
+    public String testPage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/spring-unit-testing";
+    }
+
+    @GetMapping("/Language/spring-microservice")
+    public String springMicroservicePage(Model model) {
+        // Pass any specific data to HTML page
+        return "/Language/spring-microservice";
     }
 }
