c0ea366c3aba9f8bb6cc9122397650aedbe3cb1f
k-javaman <kjavman12@gmail.com>
2023-05-29 18:42:08 +0900

    Use thymeleaf fragment to avoid repetitive hard-coded value in html

--- src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/LanguageController.java
+++ src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/LanguageController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LanguageController {
-    @GetMapping("/Language/HTML")
+    @GetMapping("/Language/html")
     public String htmlPage(Model model) {
         // Pass any specific data to HTML page
-        return "/Language/HTML";
+        return "/Language/html";
     }
 
     @GetMapping("/Language/java")
