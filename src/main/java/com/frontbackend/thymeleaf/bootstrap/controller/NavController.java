6e682a928121a48774e9932157907bce0a0cd26b
k-javaman <kjavman12@gmail.com>
2023-05-30 01:05:11 +0900

    make that guide internalizaion work

--- src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/NavController.java
+++ src/main/java/com/frontbackend/thymeleaf/bootstrap/controller/NavController.java
@@ -23,9 +23,15 @@ public class NavController {
     }
 
     @GetMapping("/changeLanguage")
-    public String changeLanguage(@RequestParam String lang) {
-        // You do not need to do anything here, as the LocaleChangeInterceptor will take care of the language change
-        return "redirect:/home";
+    public String changeLanguage(@RequestParam String lang, @RequestParam(required=false) String redirect) {
+        // LocaleChangeInterceptor will take care of the language change
+        if (redirect != null) {
+            // If a redirect page was specified, go there
+            return "redirect:" + redirect;
+        } else {
+            // If no redirect page was specified, default to home
+            return "redirect:/home";
+        }
     }
 
     // We should have this otherwise, the button is disappeared..?
