e7d151812f99f0c63ed68a31be23cd287496e603
k-javaman <121099442+k-javaman@users.noreply.github.com>
2023-05-30 08:25:58 +0900

    add persistent switch button

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -10,3 +10,16 @@ window.addEventListener('load', function() {
     const savedCheckboxState = localStorage.getItem('checkboxState');
     if (savedCheckboxState !== null) checkbox.checked = (savedCheckboxState === 'true');
 });
+
+var switch1 = document.getElementById('customSwitch1');
+
+// Save the state to localStorage whenever it changes
+switch1.addEventListener('change', function() {
+    localStorage.setItem('switchState', this.checked);
+});
+
+// When the page loads, get the state from localStorage and set the switch accordingly
+window.addEventListener('load', function() {
+    var savedState = localStorage.getItem('switchState');
+    switch1.checked = (savedState === 'true') ? true : false;
+});
--- src/main/resources/templates/en/script.js
+++ src/main/resources/templates/en/script.js
@@ -1,12 +1,44 @@
-// Store checkbox state in localStorage
-const checkbox = document.getElementById('flexSwitchCheckDefault');
+// // Store checkbox state in localStorage
+// const checkbox = document.getElementById('flexSwitchCheckDefault');
+//
+// checkbox.addEventListener('change', function() {
+//     localStorage.setItem('checkboxState', this.checked);
+// });
+//
+// // On page load, set the checkbox state to what's in localStorage
+// window.addEventListener('load', function() {
+//     const savedCheckboxState = localStorage.getItem('checkboxState');
+//     if (savedCheckboxState !== null) checkbox.checked = (savedCheckboxState === 'true');
+// });
+//
+// var switch1 = document.getElementById('customSwitch1');
+//
+// // Save the state to localStorage whenever it changes
+// switch1.addEventListener('change', function() {
+//     localStorage.setItem('switchState', this.checked);
+// });
+//
+// // When the page loads, get the state from localStorage and set the switch accordingly
+// window.addEventListener('load', function() {
+//     var savedState = localStorage.getItem('switchState');
+//     switch1.checked = (savedState === 'true') ? true : false;
+// });
 
-checkbox.addEventListener('change', function() {
-    localStorage.setItem('checkboxState', this.checked);
-});
+// get all the switch elements
+const switchElements = document.querySelectorAll('.form-check-input');
 
-// On page load, set the checkbox state to what's in localStorage
-window.addEventListener('load', function() {
-    const savedCheckboxState = localStorage.getItem('checkboxState');
-    if (savedCheckboxState !== null) checkbox.checked = (savedCheckboxState === 'true');
+// loop over all the switch elements
+switchElements.forEach(function(switchEl) {
+    const switchId = switchEl.id;
+
+    // Save the state to localStorage whenever it changes
+    switchEl.addEventListener('change', function() {
+        localStorage.setItem(switchId, this.checked);
+    });
+
+    // When the page loads, get the state from localStorage and set the switch accordingly
+    window.addEventListener('load', function() {
+        var savedState = localStorage.getItem(switchId);
+        switchEl.checked = (savedState === 'true') ? true : false;
+    });
 });
