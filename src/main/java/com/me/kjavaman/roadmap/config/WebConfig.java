f3fbf28cd993475918ccf6ce7f1ddaff4b878dbf
k-javaman <kjavaman12@gmail.com>
2023-06-03 11:53:39 +0900

    fix internalization using AcceptHeaderLocaleResolver

--- src/main/java/com/me/kjavaman/roadmap/config/WebConfig.java
+++ src/main/java/com/me/kjavaman/roadmap/config/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -17,37 +18,22 @@ import java.util.Locale;
 // This class is responsible for the configuration related to Spring MVC.
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    // This method defines the default Locale (language and country) for the application.
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US); // Set your default locale
-        return slr;
-    }
-
-    // This method creates an interceptor which will switch the language based on a lang parameter in a request.
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
-
     // Configures a {@link MessageSource} bean for message localization using properties
     // files with the base name "messages" and UTF-8 encoding.
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasenames("messages", "errors");
+        messageSource.setDefaultEncoding("utf-8");
         return messageSource;
     }
 
-    // This method registers the interceptor defined above in the application.
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
+    // This method defines the default Locale (language and country) for the application.
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US); // Set your default locale
+        return localeResolver;
     }
 
     // This method maps requests to specific paths to resources in the classpath.
--- src/main/java/com/me/kjavaman/roadmap/controller/HomeController.java
+++ src/main/java/com/me/kjavaman/roadmap/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.me.kjavaman.roadmap.controller;
 
+import com.me.kjavaman.roadmap.service.HandleByLocaleService;
 import com.me.kjavaman.roadmap.service.PageViewService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +12,14 @@ public class HomeController {
 
     private final PageViewService pageViewService;
 
-    public HomeController(PageViewService pageViewService) {
+    public HomeController(HandleByLocaleService handleByLocaleService, PageViewService pageViewService) {
         this.pageViewService = pageViewService;
     }
 
     // This method returns the view for the home page and adds a URL to the model.
     @GetMapping("/")
     public String home(Model model) {
-        Model url = pageViewService.getUrl(model);
+        Model getUrl = pageViewService.getUrl(model);
         return "home";
     }
 }
